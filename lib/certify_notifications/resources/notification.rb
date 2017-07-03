333df7325e9992d72bb66f31581f9bea8b0a60d8
Jonathan <jon.dandois@gmail.com>
2017-07-03 14:07:35 -0400

    HUB-605 updating error handling on notifications

--- lib/certify_notifications/resources/notification.rb
+++ lib/certify_notifications/resources/notification.rb
@@ -10,7 +10,7 @@ module CertifyNotifications
       response = connection.request(method: :get,
                                     path: build_find_notifications_path(safe_params))
       return_response(json(response.data[:body]), response.data[:status])
-    rescue Excon::Error::Socket => error
+    rescue Excon::Error => error
       CertifyNotifications.service_unavailable error.class
     end
 
@@ -24,25 +24,29 @@ module CertifyNotifications
                                     body: safe_params.to_json,
                                     headers:  { "Content-Type" => "application/json" })
       return_response(json(response.data[:body]), response.data[:status])
-    rescue Excon::Error::Socket => error
+    rescue Excon::Error => error
       CertifyNotifications.service_unavailable error.class
     end
 
     # update notification status
-    def self.update(params)
+    def self.update(params = nil)
+      return CertifyNotifications.bad_request if empty_params(params)
       safe_params = notification_safe_params params
-      return return_response("Invalid parameters submitted", 422) if safe_params.empty? && !params.empty?
+      return CertifyNotifications.unprocessable if safe_params.empty?
       response = connection.request(method: :put,
                                     path: build_update_notification_path(safe_params),
                                     body: safe_params.to_json,
                                     headers:  { "Content-Type" => "application/json" })
-      body = response.data[:body].empty? ? { message: 'No Content' } : json(response.data[:body])
-      return_response(body, response.data[:status])
-    rescue Excon::Error::Socket => error
-      return_response(error.message, 503)
+      return_response(check_empty_body(response.data[:body]), response.data[:status])
+    rescue Excon::Error => error
+      CertifyNotifications.service_unavailable error.class
     end
 
     private_class_method
+
+    def self.check_empty_body(body)
+      body.empty? ? { message: 'No Content' } : json(body)
+    end
 
     # helper for white listing parameters
     def self.notification_safe_params(params)
