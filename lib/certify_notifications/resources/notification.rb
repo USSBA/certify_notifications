0c86bb83a0de3019dd1f7a6184b5a2fb9e957866
Tyler Bolchoz <tybolt@gmail.com>
2017-08-24 00:15:00 -0400

    wow really

--- lib/certify_notifications/resources/notification.rb
+++ lib/certify_notifications/resources/notification.rb
@@ -9,8 +9,7 @@ module CertifyNotifications
       return CertifyNotifications.unprocessable if safe_params.empty?
       response = connection.request(method: :get,
                                     path: build_find_notifications_path(safe_params))
-      return_response(json(response.data[:body]), response.data[:page], response.data[:per_page], response.data[:total_pages], response.data[:status])
-      byebug
+      return_response(json(response.data[:body]), json(response.data[:page]), json(response.data[:per_page]), json(response.data[:total_pages]), response.data[:status])
     rescue Excon::Error => error
       CertifyNotifications.service_unavailable error.class
     end
